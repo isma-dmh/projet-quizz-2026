cacfcf0007f5beabfd06b548d3df8789b081f3e3
ismail <ismaildimah@gmail.com>
2026-05-04 19:16:15 +0200

    Ajout page entrainement

---
+++ src/pages/Entrainement.jsx
@@ -0,0 +1,28 @@
+import { Jeu } from "../components/Jeu";
+import { PaysContext } from "../context/PaysContext";
+import { useContext, useState } from "react";
+import { ResultatEntraiment } from "../components/ResultatEntrainement";
+
+export const Entrainement = () => {
+   const { pays, loading } = useContext(PaysContext);
+  const [score, setScore] = useState(0);
+  const [numeroQuestion, setNumeroQuestion] = useState(1);
+
+  if (loading) return <p>Chargement ... </p>;
+
+  if (numeroQuestion > 10) return <ResultatEntraiment score={score} numeroQuestion={numeroQuestion} />;
+
+  return (
+    <div className="bloc-jeu jeu-normal">
+      <p>{score}</p>
+      <Jeu
+        pays={pays}
+        score={score}
+        setScore={setScore}
+        numeroQuestion={numeroQuestion}
+        setNumeroQuestion={setNumeroQuestion}
+        mode={"entrainement"}
+      />
+    </div>
+  );
+}
