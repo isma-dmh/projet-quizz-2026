d11404b4a6c0df901ad12f56036dce6040a3646c
ismail <ismaildimah@gmail.com>
2026-05-04 19:01:08 +0200

    Ajout timer

--- src/pages/Chrono.jsx
+++ src/pages/Chrono.jsx
@@ -1,28 +1,38 @@
 import { Jeu } from "../components/Jeu";
 import { PaysContext } from "../context/PaysContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Resultat } from "../components/Resultat";
 
 export const Chrono = () => {
   const { pays, loading } = useContext(PaysContext);
   const [score, setScore] = useState(0);
   const [numeroQuestion, setNumeroQuestion] = useState(1);
+  const [time, setTime] = useState(60);
+
+  useEffect(() => {
+    if (loading || time <= 0) return;
+
+    const newTime = setInterval(() => setTime((t) => t - 1), 1000);
+
+    return () => clearInterval(newTime);
+  }, [loading, time]);
 
   if (loading) return <p>Chargement ... </p>;
 
-  if (numeroQuestion > 10)
+  if (time === 0)
     return <Resultat score={score} numeroQuestion={numeroQuestion} />;
 
   return (
     <div className="bloc-jeu jeu-normal">
       <p>{score}</p>
+      <p>{time}</p>
       <Jeu
         pays={pays}
         score={score}
         setScore={setScore}
         numeroQuestion={numeroQuestion}
         setNumeroQuestion={setNumeroQuestion}
-        mode ={"chrono"}
+        mode={"chrono"}
       />
     </div>
   );
