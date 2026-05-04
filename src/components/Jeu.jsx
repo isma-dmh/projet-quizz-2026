47b6b2cbc735b28b2a47f9425908ea057ecdb4c2
ismail <ismaildimah@gmail.com>
2026-05-04 18:07:01 +0200

    Ajout numero question + score

--- src/components/Jeu.jsx
+++ src/components/Jeu.jsx
@@ -3,9 +3,17 @@ import { useState, useRef, useEffect } from "react";
 
 const getRandom = (liste) => liste[Math.floor(Math.random() * liste.length)];
 
-export const Jeu = ({ pays }) => {
+export const Jeu = ({
+  pays,
+  score,
+  setScore,
+  numeroQuestion,
+  setNumeroQuestion,
+}) => {
   const paysUtilisee = useRef(null);
-  const [paysActif, setPaysActif] = useState(() => new Question(getRandom(pays), pays));
+  const [paysActif, setPaysActif] = useState(
+    () => new Question(getRandom(pays), pays),
+  );
 
   useEffect(() => {
     paysUtilisee.current = [paysActif];
@@ -15,15 +23,19 @@ export const Jeu = ({ pays }) => {
 
   const handleClick = (e) => {
     setReponseSelectionnee(e.target.value);
+
+    if (e.target.value == paysActif.bonneReponse) setScore(score + 1);
+
     setTimeout(() => {
       const paysDisponibles = pays.filter(
-        (p) => !paysUtilisee.current.some(q => q.nom === p.name),
+        (p) => !paysUtilisee.current.some((q) => q.nom === p.name),
       );
       const nouveauPays = getRandom(paysDisponibles);
-      const nouvelleQuestion = new Question(nouveauPays,pays);
+      const nouvelleQuestion = new Question(nouveauPays, pays);
       setPaysActif(nouvelleQuestion);
       paysUtilisee.current = [...paysUtilisee.current, nouvelleQuestion];
       setReponseSelectionnee(null);
+      setNumeroQuestion(numeroQuestion + 1);
     }, 1000);
   };
 
@@ -31,7 +43,9 @@ export const Jeu = ({ pays }) => {
     <div className="jeu">
       <h2>MODE NORMAL</h2>
       <img src={paysActif.flag} alt="" width={"200px"} />
-      <p>{paysActif.intitule}</p>
+      <p>
+        Question {numeroQuestion}: {paysActif.intitule}
+      </p>
       <ul>
         {paysActif.choix.map((c, id) => (
           <li key={id}>
@@ -40,7 +54,7 @@ export const Jeu = ({ pays }) => {
               value={c}
               style={{
                 color:
-                  reponseSelectionnee === c
+                  reponseSelectionnee == c
                     ? c === paysActif.bonneReponse
                       ? "green"
                       : "red"
--- src/pages/Normal.jsx
+++ src/pages/Normal.jsx
@@ -3,12 +3,21 @@ import { PaysContext } from "../context/PaysContext";
 import { useContext, useState } from "react";
 export const Normal = () => {
   const { pays, loading } = useContext(PaysContext);
+  const [score, setScore] = useState(0);
+  const [numeroQuestion, setNumeroQuestion] = useState(1);
 
-   if (loading) return <p>Chargement ... </p>;
+  if (loading) return <p>Chargement ... </p>;
 
   return (
     <div className="bloc-jeu jeu-normal">
-      <Jeu pays={pays} />
+      <p>{score}</p>
+      <Jeu
+        pays={pays}
+        score={score}
+        setScore={setScore}
+        numeroQuestion={numeroQuestion}
+        setNumeroQuestion={setNumeroQuestion}
+      />
     </div>
   );
 };
