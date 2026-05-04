dfa1ec953b1a93104cdf6bf9e08675b0bb03e138
ismail <ismaildimah@gmail.com>
2026-05-04 18:29:50 +0200

    Ajout resultat

--- src/pages/Normal.jsx
+++ src/pages/Normal.jsx
@@ -1,12 +1,16 @@
 import { Jeu } from "../components/Jeu";
 import { PaysContext } from "../context/PaysContext";
 import { useContext, useState } from "react";
+import { Resultat } from "../components/Resultat";
+
 export const Normal = () => {
   const { pays, loading } = useContext(PaysContext);
   const [score, setScore] = useState(0);
   const [numeroQuestion, setNumeroQuestion] = useState(1);
 
   if (loading) return <p>Chargement ... </p>;
+
+  if (numeroQuestion > 10) return <Resultat score={score} numeroQuestion={numeroQuestion} mode={"mode-normal"} />;
 
   return (
     <div className="bloc-jeu jeu-normal">
