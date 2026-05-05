e062c99ca12a78139e0c5f4c7a5032968155d0f3
ismail <ismaildimah@gmail.com>
2026-05-05 13:17:10 +0200

    Suppression de la limitation de 10 question

--- src/pages/Entrainement.jsx
+++ src/pages/Entrainement.jsx
@@ -1,7 +1,6 @@
 import { Jeu } from "../components/Jeu";
 import { PaysContext } from "../context/PaysContext";
 import { useContext, useState } from "react";
-import { ResultatEntrainement } from "../components/ResultatEntrainement";
 
 export const Entrainement = () => {
    const { pays, loading } = useContext(PaysContext);
@@ -9,8 +8,6 @@ export const Entrainement = () => {
   const [numeroQuestion, setNumeroQuestion] = useState(1);
 
   if (loading) return <p>Chargement ... </p>;
-
-  if (numeroQuestion > 10) return <ResultatEntrainement score={score} numeroQuestion={numeroQuestion} />;
 
   return (
     <div className="bloc-jeu jeu-normal">
